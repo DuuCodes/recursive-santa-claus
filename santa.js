ad1303587af4c55b71dad769d17723da8a975607
Dawdu <dawduwrites@gmail.com>
2019-09-25 21:33:09 -0400

    updated var on first and rest on santa.js

--- santa.js
+++ santa.js
@@ -1,8 +1,8 @@
 function deliverPresents(houses) {
   if (houses.length === 0) return 0 
   
-  first = houses[0]
-  rest = houses.slice(1, houses.length)
+  var first = houses[0]
+  var rest = houses.slice(1, houses.length)
   console.log('Delivering presents to ' + houses[0]) 
   deliverPresents(rest)
 }
